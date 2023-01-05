dbba75b0419ead05f0d0f8337abc62992259608f
amansoni <amansoni2041999@gmail.com>
2023-01-05 22:23:59 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    internal class SnakeLadder
+    {
+        public static void WelcomeMsg()
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+        }
+        static void Main(string[] args)
+        {
+            WelcomeMsg();
+            int position = 0;
+            Console.WriteLine($"Initial starting position of Player :{position}");
+            Console.ReadLine();
+
+        }
+            
+        
+        
+
+
+        
+    }
+}
